3c848b427a641d6083401021b2713a2f3c4f2ec3
matheus costa <mf.costa@live.com>
2017-08-14 19:13:49 -0300

    remove node modules

--- node_modules/react-native-circular-progress/index.js
+++
@@ -1,5 +0,0 @@
-import CircularProgress from './src/CircularProgress';
-import AnimatedCircularProgress from './src/AnimatedCircularProgress';
-
-exports.CircularProgress = CircularProgress;
-exports.AnimatedCircularProgress = AnimatedCircularProgress;
